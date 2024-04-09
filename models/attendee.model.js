25c703078457fa98377abd4c9bb8063aa0caff5c
Developer-vansh <117535964+Developer-vansh@users.noreply.github.com>
2024-04-09 16:05:08 +0530

    Added bcrypt password hasing and verifying methods in model of attendee

--- models/attendee.model.js
+++ models/attendee.model.js
@@ -1,6 +1,7 @@
 // Require Mongoose
 import { Schema, model } from "mongoose";
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 // Define the Attendee schema
 const AttendeeSchema = new Schema({
   name: {
@@ -20,10 +21,24 @@ const AttendeeSchema = new Schema({
     type: String,
     required: true,
   },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  refreshToken: {
+    type: String,
+  },
 });
-
-// Create the Attendee model
-const Attendee = model("Attendee", AttendeeSchema);
+AttendeeSchema.pre("save", async function (next) {
+  // Hash the password before saving the Attende model only when the password is modified or new
+if (this.isModified("password")) {
+  this.password = await bcrypt.hash(this.password, 10);
+}
+next();
+});
+AttendeeSchema.methods.isPasswordCorrect = async function (password) {
+return await bcrypt.compare(password, this.password);
+};
 
 // Export the Attendee model
 export default Attendee;

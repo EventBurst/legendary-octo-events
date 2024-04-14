f08dd71cb67c684e70ea5dd1aa70978ebd3c1158
Nikhil Sarwara <nikhil4818.be22@chitkara.edu.in>
2024-04-14 09:40:25 +0530

    add home page Resolves #11 @Nikhil-Sarwara

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ import YAML from "yamljs";
 // import routes
 import attendeRoutes from './routes/attende.routes.js'
 
+import { fileURLToPath } from "url";
+
+// Path
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const swaggerDocument = YAML.load("./swagger.yaml");
 
 const app = express();
@@ -21,17 +29,17 @@ app.use(express.static("public"));
 // for cookies parsing and setting
 app.use(cookieParser());
 app.use(cors());
-//Server Working
+// Define route to serve the index.html file
 app.get("/", (_, res) => {
-  res.send("HLO");
+  res.sendFile(path.join(__dirname, "index.html")); // Send the index.html file
 });
 
 //Routes
-app.use("/api/legendry-octo-events/attendee", attendeRoutes);
+app.use("/api/legendary-octo-events/attendee", attendeRoutes);
 
 // Swagger UI
 app.use(
-  "/api/shiny-barnacle/docs",
+  "/api/legendary-octo-events/docs",
   swaggerUi.serve,
   swaggerUi.setup(swaggerDocument)
 );
